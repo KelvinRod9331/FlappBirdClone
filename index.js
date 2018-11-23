f5e7f78a325937b8f6d41e6d78470f8f3af45964
KelvinRod9331 <kelvinrodriguez@ac.c4q.nyc>
2018-11-23 15:17:36 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var bird
+
+function setup(){
+    createCanvas(400, 600)
+    bird = new Bird()
+}
+
+function draw(){
+    background(0)
+    bird.show()
+    bird.update()
+}
+
+function keyPressed(){
+    if(key === ' '){
+        bird.up()
+    }
+}
+
+setup()
+draw()
